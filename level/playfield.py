f742d60262a0eeb41ad428b57e9abd9ce2605266
thismarvin <bend_@outlook.com>
2019-06-09 23:11:50 -0500

    added input class

--- level/playfield.py
+++ level/playfield.py
@@ -1,15 +1,25 @@
 from pygame.math import Vector2
 from entities.geometry.rectangle import Rectangle
 from utilities.color import Color
+from utilities.input import Input
+from utilities.input import InputType
 
 
 class Playfield:
     def __init__(self, camera):
         self.camera = camera
-        self.camera_location = Vector2(0,0)
+        self.camera_location = Vector2(0, 0)
+        self.input = Input()
+
+    def update_camera(self):
+        self.camera.update()
+
+    def update_input(self, delta_time):
+        self.input.update()
 
     def update(self, delta_time):
-        self.camera.update()
+        self.update_camera()
+        self.update_input()
 
     def draw(self, surface):
         ""
---
+++ utilities/input.py
@@ -0,0 +1,29 @@
+import pygame
+from enum import Enum
+
+
+class InputType(Enum):
+    NONE = 0
+    UP = 1
+    LEFT = 2
+    DOWN = 3
+    RIGHT = 4
+
+
+class Input:
+    def __init__(self):
+        self.key_state = None
+
+    def pressing(self, type=InputType.NONE):
+        if type == InputType.UP:
+            return self.key_state[pygame.K_UP] or self.key_state[pygame.K_w]
+        if type == InputType.LEFT:
+            return self.key_state[pygame.K_LEFT] or self.key_state[pygame.K_a]
+        if type == InputType.DOWN:
+            return self.key_state[pygame.K_DOWN] or self.key_state[pygame.K_s]
+        if type == InputType.RIGHT:
+            return self.key_state[pygame.K_RIGHT] or self.key_state[pygame.K_d]
+        return False
+
+    def update(self):
+        self.key_state = pygame.key.get_pressed()
