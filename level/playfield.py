f0281c98f9623e460c4e733a8cb993724212553a
thismarvin <bend_@outlook.com>
2019-06-12 12:48:03 -0500

    added an Entity List in Playfield

--- level/playfield.py
+++ level/playfield.py
@@ -1,4 +1,4 @@
-from entities.geometry.rectangle import Rectangle
+from entities.user.player import Player
 from utilities.Vector2 import Vector2
 from utilities.color import Color
 from utilities.input import Input
@@ -7,19 +7,33 @@ from utilities.cameras import Camera
 
 
 class Playfield:
+    ENTITIES = []
+
     def __init__(self):
+        self.camera = Camera()
         self.camera_location = Vector2(0, 0)
         self.input = Input()
+        self.reset()
+
+    def reset(self):
+        Playfield.ENTITIES = []
+        Playfield.ENTITIES.append(Player(10, 10, 10, 10))
 
     def update_camera(self):
-        Camera.update()
+        self.camera.update()
 
     def update_input(self):
         self.input.update()
 
+    def update_entities(self, delta_time):
+        for i in range(len(Playfield.ENTITIES) - 1, -1, -1):
+            Playfield.ENTITIES[i].update(delta_time)
+
     def update(self, delta_time):
         self.update_camera()
         self.update_input()
+        self.update_entities(delta_time)
 
     def draw(self, surface):
-        ""
+        for i in range(len(Playfield.ENTITIES)):
+            Playfield.ENTITIES[i].draw(surface)
