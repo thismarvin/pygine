ab10dd436b609a0a359f93556dc15d9977518974
thismarvin <bend_@outlook.com>
2019-06-12 12:45:19 -0500

    added more controls

--- utilities/input.py
+++ utilities/input.py
@@ -4,12 +4,21 @@ from enum import Enum
 
 class InputType(Enum):
     NONE = 0
+
     UP = 1
     LEFT = 2
     DOWN = 3
     RIGHT = 4
-    TOGGLE_FULLSCREEN = 5
-    QUIT = 6
+    A = 5
+    B = 6
+    X = 7
+    Y = 8
+    START = 9
+    SELECT = 10    
+
+    RESET = 11
+    TOGGLE_FULLSCREEN = 12
+    QUIT = 13
 
 
 class Input:
@@ -25,10 +34,26 @@ class Input:
             return self.key_state[pygame.K_DOWN] or self.key_state[pygame.K_s]
         if type == InputType.RIGHT:
             return self.key_state[pygame.K_RIGHT] or self.key_state[pygame.K_d]
+        if type == InputType.A:
+            return self.key_state[pygame.K_j]
+        if type == InputType.B:
+            return self.key_state[pygame.K_k]
+        if type == InputType.X:
+            return self.key_state[pygame.K_u]
+        if type == InputType.Y:
+            return self.key_state[pygame.K_i]
+        if type == InputType.SELECT:
+            return self.key_state[pygame.K_SPACE]
+        if type == InputType.START:
+            return self.key_state[pygame.K_KP_ENTER]
+
+        if type == InputType.RESET:
+            return self.key_state[pygame.K_r]
         if type == InputType.TOGGLE_FULLSCREEN:
             return self.key_state[pygame.K_F10]
         if type == InputType.QUIT:
-            return self.key_state[pygame.K_ESCAPE]
+            return self.key_state[pygame.K_ESCAPE] or self.key_state[pygame.K_BACKSPACE]
+            
         return False
 
     def update(self):
