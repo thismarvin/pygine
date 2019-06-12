6bd6842dcbd74b11335bce907d04111e7ec16872
thismarvin <bend_@outlook.com>
2019-06-12 12:48:08 -0500

    misc

--- ui/menu.py
+++ ui/menu.py
@@ -1,12 +1,9 @@
-from utilities.color import Color
-
-
 class Menu:
     def __init__(self):
-        ""
+        pass
 
     def update(self, delta_time):
-        ""
+        pass
 
     def draw(self, surface):
-        ""
+        pass
