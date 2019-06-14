ba4202c70e9c1169abdda47db67f295028dd292a
Marvin Valenzuela <bend_@outlook.com>
2019-06-14 12:37:52 -0500

    added pinhole class

--- entities/geometry/circle.py
+++ entities/geometry/circle.py
@@ -17,6 +17,13 @@ class Circle(Entity):
         self.bounds = pygame.Rect(
             self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
 
+    def set_thickness(self, thickness):
+        self.thickness = thickness
+        if self.thickness < 0:
+            self.thickness = 1
+        if self.thickness > self.radius:
+            self.thickness = self.radius
+
     def draw(self, surface):
-        pygame.draw.circle(surface, Color.RED, (int(self.scaled_location().x), int(
+        pygame.draw.circle(surface, self.color, (int(self.scaled_location().x), int(
             self.scaled_location().y)), int(self.radius * Camera.SCALE), int(self.thickness * Camera.SCALE))
