1aae9ccb148b20a918e8ef21d84b20d544a73031
thismarvin <bend_@outlook.com>
2019-06-14 20:05:15 -0500

    showcase new features

--- level/hud.py
+++ level/hud.py
@@ -1,9 +1,13 @@
 from utilities.transitions import Pinhole
 from utilities.transitions import PinholeType
+from utilities.camera import CameraType
 
 
 class HUD():
     def __init__(self):
+        self.pinhole = Pinhole(PinholeType.OPEN)
+
+    def reset(self):
         self.pinhole = Pinhole(PinholeType.OPEN)
 
     def update(self, delta_time):
