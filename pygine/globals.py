2795f86688092d44fe8feecfdf49d2fed5f668ed
thismarvin <bend_@outlook.com>
2019-08-26 20:34:40 -0500

    fixed spelling mistake

--- pygine/globals.py
+++ pygine/globals.py
@@ -1,2 +1,2 @@
-# Yeah alright global varibles are bad, but give me a break im only using one and it's for debugging purposes!
+# Yeah alright global variables are bad, but give me a break im only using one and it's for debugging purposes!
 debugging = False
