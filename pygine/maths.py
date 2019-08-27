155ab46352de0b02eb482ab618e0f87c2a27de74
thismarvin <bend_@outlook.com>
2019-08-27 15:44:36 -0500

    added useful vector methods

--- pygine/maths.py
+++ pygine/maths.py
@@ -6,8 +6,19 @@ def distance_between(vector_a, vector_b):
 
 
 class Vector2:
-    "a poor man's vector class"
+    "a poor man's vector class."
 
     def __init__(self, x=0.0, y=0.0):
         self.x = x
         self.y = y
+
+    def length(self):
+        "Returns the length of this vector."
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def normalize(self):
+        "Converts this vector into a unit vector."
+        magnitude = self.length()
+        if magnitude > 0:
+            self.x /= magnitude
+            self.y /= magnitude
